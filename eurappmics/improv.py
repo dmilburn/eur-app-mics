51545f496c0367051c0bea9754614e8d0d8f306a
mintchkin <mintchkin@server.fake>
2016-08-18 10:04:54 -0400

    simple_rhythm now takes a TimeSignature object

--- eurappmics/improv.py
+++ eurappmics/improv.py
@@ -1,18 +1,26 @@
-from music21 import note, stream
+from music21 import note, stream, meter
 import random
 
 
-def simple_rhythm(length=1):
-    basic = [1, 1/2, 1/4]
-    compound = [3/4]
-    triplet = [1/3]
-    durations = basic+compound+triplet
+def simple_rhythm(ts = meter.TimeSignature('4/4')):
+    ## TODO: Just generate a list of rhythmic vocab and randomly choose from that
+    ql = ts.beatDuration.quarterLength
+    tl = ts.totalLength
+    basic = [ql*(x+1) for x in range(int(tl/ql))]
+    subdivisions = [.25*(x+1) for x in range(int(ql/.25))]
+    triplet = [x/3 for x in basic if x not in basic+subdivisions]
+    durations = list(set(basic+subdivisions+triplet))
 
-    rhythm = stream.Stream()
-    while(length):
+    rhythm = stream.Measure()
+    rhythm.timeSignature = ts
+    while(tl):
         n = note.Note(quarterLength=random.choice(durations))
-        length -= n.quarterLength
-        rhythm.append(n)
-        durations = [d for d in durations
-                     if length-d in durations or length-d == 0]
+        if n.quarterLengthFloat in triplet:
+            tl -= n.quarterLength*3
+            rhythm.repeatAppend(n, 3)
+        else:
+            tl -= n.quarterLength
+            rhythm.append(n)
+        durations = [d for d in set(basic+subdivisions+triplet)
+                     if tl-d >= 0]
     return rhythm

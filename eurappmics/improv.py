df554c5fca2d4ae21840cab78e4bb7e28d0ec87c
Debbie Milburn <deborah.a.milburn@gmail.com>
2017-03-02 20:43:58 -0500

    Create melody improv

--- eurappmics/improv.py
+++ eurappmics/improv.py
@@ -1,5 +1,13 @@
-from music21 import note, stream, meter
+from music21 import note, stream, meter, pitch, scale
 import random
+
+def simple():
+    i = 0
+    rhythms = stream.Part()
+    while i < 100:
+        rhythms.append(simple_rhythm())
+        i += 1
+    return simple_melody(rhythms)
 
 def simple_rhythm(ts = meter.TimeSignature('4/4')):
     ## TODO: Just generate a list of rhythmic vocab and randomly choose from that
@@ -22,3 +30,21 @@ def simple_rhythm(ts = meter.TimeSignature('4/4')):
         durations = [d for d in set(basic+subdivisions) if tl-d >= 0] + \
                     [d for d in set(triplet) if tl-(d*3) >= 0]
     return rhythm
+
+def simple_melody(melody, key='c'):
+    hbscale = scale.WeightedHexatonicBlues(key)
+    length_of_chain = 6
+    chain = 0
+    direction = 'ascending'
+    next_pitch = hbscale.next()
+    for measure in melody.measures(0,None):
+        for note in measure.notes:
+            if chain == length_of_chain:
+                chain = 0
+                direction = 'ascending' if direction == 'descending' else 'descending'
+
+            next_pitch = hbscale.next(next_pitch, direction)
+            note.pitch = next_pitch
+            chain += 1
+
+    return melody

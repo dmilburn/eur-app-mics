50162fafff24817e8d7eebf41eb1776fedaa80b3
Debbie Milburn <debbiemilburn2@gmail.com>
2017-03-16 15:18:00 -0400

    Update simple_rhythm

--- eurappmics/improv.py
+++ eurappmics/improv.py
@@ -1,21 +1,19 @@
 from music21 import note, stream, meter
 import random
 
-
 def simple_rhythm(ts = meter.TimeSignature('4/4')):
     ## TODO: Just generate a list of rhythmic vocab and randomly choose from that
     ql = ts.beatDuration.quarterLength
-    tl = ts.totalLength
+    tl = ts.barDuration.quarterLength
     basic = [ql*(x+1) for x in range(int(tl/ql))]
     subdivisions = [.25*(x+1) for x in range(int(ql/.25))]
     triplet = [x/3 for x in basic if x/3 not in basic+subdivisions]
     durations = list(set(basic+subdivisions+triplet))
-
     rhythm = stream.Measure()
     rhythm.timeSignature = ts
     while(tl):
         n = note.Note(quarterLength=random.choice(durations))
-        if n.quarterLengthFloat in triplet:
+        if float(n.quarterLength) in triplet:
             tl -= n.quarterLength*3
             rhythm.repeatAppend(n, 3)
         else:
